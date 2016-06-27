364ca65b5e69e8f31d64de70d076fdab7c7ea5b9
SeeYouTomorrow <li_ning_@outlook.com>
2016-06-27 23:33:57 +0800

    系数矩阵的一般转置新增

--- 稀疏矩阵和快速转置/SparseMatrix.h
+++ 稀疏矩阵和快速转置/SparseMatrix.h
@@ -68,6 +68,36 @@ public:
 		cout<<endl;
 	}
 
+	//系数矩阵的一般转置
+	SparseMatrix<T> Transpose()
+	{
+		SparseMatrix<T> sm;
+		sm._colSize = _rowSize;
+		sm._rowSize = _colSize;
+		sm._invalid = _invalid;
+
+		// O(colSize*有效数据的个数)
+		for (size_t i = 0; i < _colSize; ++i)
+		{
+			size_t index = 0;
+			while (index < _array.size())
+			{
+				if (_array[index]._col == i)
+				{
+					Trituple<T> t;
+					t._row = _array[index]._col;
+					t._col = _array[index]._row;
+					t._value = _array[index]._value;
+					sm._array.push_back(t);
+				}
+
+				++index;
+			}
+		}
+
+		return sm;
+	}
+
 private:
 	//Trituple* _array;	// 压缩存储
 	//size_t _size;		// 稀疏矩阵的有效数据的个数
@@ -92,4 +122,7 @@ void Test2()
 
 	SparseMatrix<int> sm1((int*)a, 6, 5, 0);
 	sm1.Display();
+
+	SparseMatrix<int> sm2 = sm1.Transpose();
+	sm2.Display();
 }
