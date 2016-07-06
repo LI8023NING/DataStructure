11d1bc42cfc9580ddc27fed026ffaa60c4c9acd6
SeeYouTomorrow <li_ning_@outlook.com>
2016-07-06 15:41:15 +0800

    添加二叉树的广度优先搜索和深度优先搜索

--- 求二叉树中两个节点的最远距离/SearchBinaryTree.h
+++ 求二叉树中两个节点的最远距离/SearchBinaryTree.h
@@ -1,4 +1,6 @@
 #pragma once
+#include<stack>
+#include<queue>
 
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
@@ -157,6 +159,55 @@ public:
 		max_len = max(left_len + right_len - 1, max_len);
 	}
 
+	//深度优先遍历
+	//因此可以借助堆栈的数据结构，由于堆栈是后进先出的顺序，
+	//由此可以先将右子树压栈，然后再对左子树压栈，
+	//这样一来，左子树结点就存在了栈顶上，因此某结点的左子树能在它的右子树遍历之前被遍历。
+	void depthFirstSearch(Node* root)
+	{
+		stack<Node*> TreeNode;
+		TreeNode.push(root);        //根节点入栈
+		Node* node = nullptr;
+		while (!TreeNode.empty())
+		{
+			node = TreeNode.top();
+			cout << node->key << " "; //访问
+			TreeNode.pop();
+
+			if (node->_right)         //右子树先入栈
+				TreeNode.push(node->_right);
+
+			if (node->_left)          //左子树再入栈
+				TreeNode.push(node->_left);
+		}
+	}
+
+	//广度优先搜索
+	//是从根节点开始，沿着树的宽度遍历树的节点。如果所有节点均被访问，则算法中止。
+	//类似层序遍历，需要借助队列
+	//广度优先遍历
+	void breadthFirstSearch(Node* root)
+	{
+		if (root == nullptr)
+			return;
+
+		queue<Node*> TreeNode;
+		Node* node = nullptr;
+
+		TreeNode.push(root);
+		while (!TreeNode.empty())
+		{
+			node = TreeNode.front();
+			cout << node->key << " ";
+			TreeNode.pop();
+
+			if (node->_left)             //先左再右，一次性入队，在从头出队，一层层遍历
+				TreeNode.push(node->_left);
+			if (node->_right)
+				TreeNode.push(node->_right);
+		}
+	}
+
 public:
 	SBTNode<K, V> *_root;
 };
@@ -174,5 +225,8 @@ void Test()
 	s1.InOrder(s1._root);
 	cout << endl;
 	cout<<s1.find_max_len(s1._root)<<endl;
+	s1.depthFirstSearch(s1._root);
+	cout << endl;
+	s1.breadthFirstSearch(s1._root);
 	
 }
